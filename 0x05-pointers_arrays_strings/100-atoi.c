4d0af8accaaa3a1a4c6c6bb393b9ab46094469c8
annieakpan <anniebakpan@gmail.com>
2022-10-24 11:06:19 -0700

    strings to integres

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to convert
+ *
+ * Return: interger value
+ */
+
+int _atoi(char *s)
+{
+
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, d = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+			{
+				d *= 10;
+				size++;
+			}
+		}
+		count++;
+
+		for (i = count - size; i < count; i++)
+		{
+			oi = oi + ((*(s + i) - 48) * d);
+			d /= 10;
+		}
+		return (oi * pn);
+	}
