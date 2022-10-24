66ffd97bf304842770c6252fcf4b2607d4712fd1
annieakpan <anniebakpan@gmail.com>
2022-10-24 10:41:13 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string - Reverses a string
+ * @s: the string to reverse
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	char temp;
+
+	for (i = 0; i < _strlen(s) / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[_strlen(s) - i - 1];
+		s[_strlen(s) - i - 1] = temp;
+	}
+}
