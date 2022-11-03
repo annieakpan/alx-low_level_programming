0e67832087d2d6c04cbfcb3d9112c17ed51263c3
annieakpan <anniebakpan@gmail.com>
2022-11-03 05:55:51 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string using recursion
+ * @s: string to recurse through
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
